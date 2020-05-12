ceaa1158a97dce3b2613996166d38edfe49e48d5
ricardofqueiroz <49354222+ricardofqueiroz@users.noreply.github.com>
2020-05-11 21:30:33 -0300

    Alterado comentários

--- exArray.php
+++ exArray.php
@@ -1,4 +1,4 @@
-<p>Verificar se um nome existe dentro de uma array com o uso de in_array</p>
+<p>Verificar se um nome existe dentro de uma array</p>
 <form action="exArray.php" method="POST">
 Digite um nome <input type="text" name="pessoa">
 <input type="submit">
@@ -6,34 +6,30 @@ Digite um nome <input type="text" name="pessoa">
 <?php
 /*Ocultar erro para o usuário*/
 /*
-Para ocultar, coloque o nº zero ou boleno "false".
+Para ocultar, coloque o nº zero ou boleano "false".
 Para não ocultar, coloque o nº um ou boleano "true".
 */
 ini_set('display_errors',false);
 //===================================================================//
+//Construindo duas arrays
 $nomes=array('Alex', 'Beatriz', 'Cassia', 'Denis', 'Evandro', 'Fabio');
 $nomes2=array('Gustavo', 'Henrique', 'Igor', 'João', 'Luan', 'Mateus');
-//if(isset($_POST["pessoa"]));
+//if(isset($_POST["pessoa"])); - Informa se a variável foi definida
 $pessoa=$_POST["pessoa"];
 //$contar=0;
 //if (in_array($_POST['pessoa'], $nomes))
-if (in_array($pessoa, $nomes)) //sintaxe
+if (in_array($pessoa, $nomes))
 {
 echo "Pertence a 1º lista de nomes";
-//echo "<script>alert ('Pertence a 1º lista de nomes' )</script>";
 }
 else if (in_array($pessoa, $nomes2))
 {
 echo "Pertence a 2º lista de nomes";
-//echo "<script>alert ('Pertence a 2º lista de nomes')</script>";
 }
-else //if (is_null($pessoa))
+else //if (is_null($pessoa)) - Sintaxe que informa se a variável de entrada esta vazia
 {
 echo "Esse nome não pertence a nenhuma lista ou foi digitado nada"; echo "<br>";
 echo "Digite novamente";
-//echo "<script>alert ('Esse nome não pertence a nenhuam lista')</script>";
 }
-//sleep(3);
-//echo "<br>";
 //var_dump($_POST);
 ?>
