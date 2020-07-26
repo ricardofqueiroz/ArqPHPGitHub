d273294ce1840d88fc37d529bc3bb4a5e620974a
ricardofqueiroz <ricardoqueiroz81@hotmail.com>
2020-07-26 20:43:13 -0300

    Inseridono arquivo exemplo de criar atributos privado.

--- exClassObj.php
+++ exClassObj.php
@@ -1,3 +1,4 @@
+<p>EXEMPLO DE CRIAÇÃO DE CLASSE</p>
 <form method="POST">
 	<p>Digite os produtos</p>
 <input type="text" name="Produto1">
@@ -40,4 +41,126 @@ $Compras->Produto3=$Produto3;
 
 //Informar e referenciar os atributos que serão comprados da função comprar
 echo $Compras->comprar();
+?>
+<P>EXEMPLO DE CRIAÇÃO DE ATRIBUTOS PRIVADO</p>
+<p>COMPONENTES DO COMPUTADOR</p>
+<form method="POST">
+    Tipo de processador
+    <input type="text" name="processador">
+    <br>
+    Tamanho do Disco Rigido 
+    <input type="text" name="DiscoRigido">
+    <br>
+    Tamanho da mémoria RAM
+    <input type="text" name="memoria">
+    <br>
+    Tipo de entrada
+    <input type="text" name="entrada">
+    <br>
+    <input type="submit" value="inserir">
+</form>
+<?php
+ini_set('display_errors', false);
+$processador=$_POST['processador'];
+$DiscoRigido=$_POST['DiscoRigido'];
+$memoria=$_POST['memoria'];
+$entrada=$_POST['entrada'];
+//Criando atributos privados
+
+class Computador
+{
+	//Atributos privados do computador
+	private $processador;
+	private $DiscoRigido;
+	private $memoria;
+	private $entrada;
+
+	/*
+	Criando funções para esses atributos com get e set.
+	A função get vai pegar o valor do atributo
+	A função set vai definir o valor do atributo 
+    */
+
+	//Criar um método para funçao do processador para pegar o valor
+	public function getProcessador()
+	{
+		// Retornar referenciando o atributo processador
+		return $this->processador;
+	}
+	//Criar um método para função do processador para definir o valor
+	public function setProcessador($processador) 
+    /*
+	Obs. O atributo "$processador" é usado como parametro para ser definido o valor, não é preciso ser retornado pois o set esta definindo, se precisar chamar usa-se o return
+	*/
+	{
+	    $this->processador=$processador;
+	}
+	
+	//Método para função DiscoRigido
+	public function getDiscoRigido()
+	{
+		return $this->DiscoRigido;
+	}
+	public function setDiscoRigido($DiscoRigido)
+	{
+		$this->DiscoRigido=$DiscoRigido;
+	}
+    
+    //Método para função memório
+    public function getMemoria()
+    {
+    	return $this->memoria;
+    }
+    public function setMemoria($memoria)
+    {
+    	$this->memoria=$memoria;
+    }
+
+    //Método para função entrada
+    public function getEntrada()
+    {
+    	return $this->entrada;
+    }
+    public function setEntrada($entrada)
+    {
+    	$this->entrada=$entrada;
+    } 
+    /*
+    Criando um método pra exibir todas as informações dos atributos "$processador, $DiscoRigido, $memoria e $entrada".
+    Será usado uma array junto com a função get pra pegar as informações.
+    */
+    public function ExibirInformacao()
+    {
+    	//Retornar uma array para exibir informações
+    	return array(
+    		"processador"=>$this->getProcessador(),
+    		"DiscoRigido"=>$this->getDiscoRigido(),
+    		"memoria"=>$this->getMemoria(),
+    		"entrada"=>$this->getEntrada(),
+    	);
+    }
+
+}
+//Criando um objeto para chamar a classe "Computador"
+$tecnologia=new Computador();
+//Instanciando e definindo os valores de cada atributo
+/*
+$tecnologia->setProcessador("intel Core i7");
+$tecnologia->setDiscoRigido("500 GB");
+$tecnologia->setMemoria("4 GB");
+$tecnologia->setEntrada("USB, HDMI");
+*/
+$tecnologia->setProcessador($processador);
+$tecnologia->setDiscoRigido($DiscoRigido);
+$tecnologia->setMemoria($memoria);
+$tecnologia->setEntrada($entrada);
+
+//Visualizar na tela só as innformações sem array
+//echo $tecnologia->getProcessador().", " .$tecnologia->getDiscoRigido().", " .$tecnologia->getMemoria().", " .$tecnologia->getEntrada();
+
+//Visualizar na tela as informações com array
+echo "Infomaççoes dos componentes do computador";
+echo "<br>";
+//echo var_dump($tecnologia->ExibirInformacao());
+print_r($tecnologia->ExibirInformacao());
 ?>
